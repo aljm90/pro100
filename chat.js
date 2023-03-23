dd0ee4efcfba12ad303b26c215cea371a9791576
aljm90 <117952840+aljm90@users.noreply.github.com>
2023-03-23 17:00:37 -0600

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,27 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD823LUPaopnQ8H_o4u6I-dGhu_WSzzFe0",
+    authDomain: "p100-8e391.firebaseapp.com",
+    databaseURL: "https://p100-8e391-default-rtdb.firebaseio.com",
+    projectId: "p100-8e391",
+    storageBucket: "p100-8e391.appspot.com",
+    messagingSenderId: "1045134285094",
+    appId: "1:1045134285094:web:e71a7b9745a2aa5bdc49e7"
+  };
+firebase.initializeApp(firebaseConfig);{
+
+    localStorage.setItem("user_name", user_name);
+
+    window.location = "chat_room.html";
+
+
+}
+
+
+
+    
+    
+   
+
+
+
+
